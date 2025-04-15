de8b1a4cb93bfbf7f4630e9bdfa18f6d8bfe8acc
YAUHENI (Eugene) PASHKELEVICH <Yauheni.Pashkelevich@ask.com>
2025-04-15 02:34:34 -0500

    подсократил

--- src/main/java/com/my/buyrecipes/Main.java
+++ src/main/java/com/my/buyrecipes/Main.java
@@ -4,6 +4,8 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.util.*;
 
 public class Main {
@@ -22,46 +24,33 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         String path = args.length > 0 ? args[0] : "checkFolder";
-
         loadTemplates();
 
-        File folder = new File(path);
-        File[] files = folder.listFiles((dir, name) -> name.endsWith(".png"));
-
-        if (files == null) {
-            System.out.println("Folder is missing");
-            return;
-        }
+        File[] files = new File(path).listFiles((dir, name) -> name.endsWith(".png"));
+        if (files == null) throw new NoSuchFileException("File missing");
 
         for (File file : files) {
-            BufferedImage img = ImageIO.read(file);
-            String result = recognizeCards(img);
-
-            System.out.println(file.getName() + " -> " + result
-                    .replace("_gray", "")
-                    .replace("-red", "")
-                    .replace("-black", ""));
+            String result = recognizeCards(ImageIO.read(file));
+            System.out.println(file.getName() + " -> " + result.replaceAll("(_gray|-red|-black)", ""));
         }
     }
 
-    static void loadTemplates() throws Exception {
-        File rankDir = new File("templates/rank");
-        for (File f : Objects.requireNonNull(rankDir.listFiles((d, n) -> n.endsWith(".png")))) {
-            rankTemplates.put(f.getName().replace(".png", ""), ImageIO.read(f));
-        }
+    static void loadTemplates() throws IOException {
+        loadTemplateFolder("templates/rank", rankTemplates);
+        loadTemplateFolder("templates/suit", suitTemplates);
+    }
 
-        File suitDir = new File("templates/suit");
-        for (File f : Objects.requireNonNull(suitDir.listFiles((d, n) -> n.endsWith(".png")))) {
-            suitTemplates.put(f.getName().replace(".png", ""), ImageIO.read(f));
+    static void loadTemplateFolder(String path, Map<String, BufferedImage> targetMap) throws IOException {
+        for (File f : Objects.requireNonNull(new File(path).listFiles((d, n) -> n.endsWith(".png")))) {
+            targetMap.put(f.getName().replace(".png", ""), ImageIO.read(f));
         }
     }
 
-    static String recognizeCards(BufferedImage bufferedImage) throws Exception {
+    static String recognizeCards(BufferedImage bufferedImage) {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < 5; i++) {
             int x = startX + i * shift;
-
             if (x + rankWidth > bufferedImage.getWidth()) break;
 
             BufferedImage rankImg = bufferedImage.getSubimage(x, startY, rankWidth, rankHeight);
@@ -70,19 +59,17 @@ public class Main {
             String bestRank = findBestMatch(rankImg, rankTemplates);
             String bestSuit = findBestMatch(suitImg, suitTemplates);
 
-            if (bestRank != null && bestSuit != null) {
-                result.append(bestRank).append(bestSuit);
-            }
+            if (bestRank != null && bestSuit != null) result.append(bestRank).append(bestSuit);
         }
         return result.toString();
     }
 
-    static String findBestMatch(BufferedImage image, Map<String, BufferedImage> templates) throws Exception {
+    static String findBestMatch(BufferedImage image, Map<String, BufferedImage> templates) {
         String bestMatch = null;
         double minDiff = Double.MAX_VALUE;
 
         for (Map.Entry<String, BufferedImage> entry : templates.entrySet()) {
-            double diff = imageDiff(image, entry.getValue());
+            double diff = calculateImagesDifference(image, entry.getValue());
             if (diff < minDiff && diff < 15000.0) {
                 minDiff = diff;
                 bestMatch = entry.getKey();
@@ -91,19 +78,16 @@ public class Main {
         return bestMatch;
     }
 
-    static double imageDiff(BufferedImage image, BufferedImage template) {
+    static double calculateImagesDifference(BufferedImage image, BufferedImage template) {
         int w = Math.min(image.getWidth(), template.getWidth());
         int h = Math.min(image.getHeight(), template.getHeight());
         double total = 0;
 
         for (int x = 0; x < w; x++) {
             for (int y = 0; y < h; y++) {
-                Color imageColor = new Color(image.getRGB(x, y));
-                Color templateColor = new Color(template.getRGB(x, y));
-                int differenceRed = imageColor.getRed() - templateColor.getRed();
-                int differenceGreen = imageColor.getGreen() - templateColor.getGreen();
-                int differenceBlue = imageColor.getBlue() - templateColor.getBlue();
-                total += differenceRed * differenceRed + differenceGreen * differenceGreen + differenceBlue * differenceBlue;
+                Color c1 = new Color(image.getRGB(x, y));
+                Color c2 = new Color(template.getRGB(x, y));
+                total += Math.pow(c1.getRed() - c2.getRed(), 2) + Math.pow(c1.getGreen() - c2.getGreen(), 2) + Math.pow(c1.getBlue() - c2.getBlue(), 2);
             }
         }
         return total / (w * h);
